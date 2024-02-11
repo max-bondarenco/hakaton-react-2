11cc964a1a74a70126c98d6f499e36c4328abb1b
eLQeR <131271125+eLQeR@users.noreply.github.com>
2024-02-11 19:29:40 +0200

    Fixed images and else

--- auction/urls.py
+++ auction/urls.py
@@ -3,13 +3,15 @@
 from django.urls import path, include
 
 from auction.views import auction_list_detail, auction_my, auction_going, auction_going_accept, lots_my, create_auction, \
-    create_auction_success, auction_list, auction_bet
+    create_auction_success, auction_list, auction_bet, auction_refactor, auction_buy
 
 
 urlpatterns = [
     path('', auction_list, name="auction"),
     path('<int:pk>/', auction_list_detail),
+    path('<int:pk>/refactor', auction_refactor),
     path('<int:pk>/going/', auction_going),
+    path('<int:pk>/going/', auction_buy),
     path('<int:pk>/going/accepted/', auction_going_accept),
     path('<int:pk>/going/bet/', auction_bet),
     path('my-auctions/', auction_my),
